54a0f7337e5d92b227db8be36f341c3ce98ddf10
angelzou <yuweizou.cn@gmail.com>
2015-07-10 23:39:54 +0800

    backtop.js 更改

--- js/backtop.js
+++ js/backtop.js
@@ -2,41 +2,16 @@
  * Created by angelzou on 2015/7/6.
  */
 ;(function(){
-    var Top = function(){
-        this.tpl = "<div class='T-back-top hidden'><i class='arrow-up'></i></div>";
-        this._init();
-    };
-    Top.prototype = {
-        _init: function() {
-            var that = this;
-            $("body").append(this.tpl);
-            if(that.getScrollY() > 100) {
-                that.backToTop();
-            }
-            $(window).scroll(function(){
-                that.backToTop();
-                if(that.getScrollY() <= 100) {
-                    $(".T-back-top").addClass('hidden');
-                }
-            });
-        },
-        backToTop: function() {
-            if($(".T-back-top").get(0)) {
-                $(".T-back-top").removeClass('hidden');
-            } else {
-                $(body).append(this.tpl);
-                $(".T-back-top").removeClass('hidden');
-            }
-            $(".T-back-top").click(function(){
-                $(".T-back-top").addClass('hidden');
-                document.documentElement.scrollTop = document.body.scrollTop = 0;
-            });
-        },
-        getScrollY: function(){
-            //console.log(document.documentElement.scrollTop || document.body.scrollTop);
-            return document.documentElement.scrollTop || document.body.scrollTop;
-        }
-    };
-
-    var top = new Top();
+    var $el = $("<div class='T-back-top hidden'><i class='arrow-up'></i></div>")
+                .appendTo($("body")).click(function(){
+                $("html, body").animate({scrollTop: 0}, 200);   
+            }),
+        backToTop = function(){
+            var st = $(document).scrollTop();
+            st > 0 ? $el.show() : $el.hide();
+        };
+    $(window).bind("scroll", backToTop);
+    $(function(){
+        backToTop();
+    });
 })();
